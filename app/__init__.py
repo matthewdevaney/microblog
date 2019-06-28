a2e2bba47826d05640fc533693acfe5f3cd83eac
Matthew Devaney <mdevaney@fwsgroup.com>
2019-06-28 10:25:38 -0500

    Chapter 5 - Page 57

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,20 @@
+# python packages
 from flask import Flask
-from config import Config
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+# local modules
+from config import Config
+
+# initialize app
 app = Flask(__name__)
 app.config.from_object(Config)
+
+# initialize extensions
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
 
 from app import models, routes
--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
+from app import db, login
 from datetime import datetime
-from app import db
+from flask_login import UserMixin
+from werkzeug.security import check_password_hash, generate_password_hash
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -10,6 +12,17 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User {self.username}>'
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    @login.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
